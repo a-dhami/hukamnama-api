7c789aabb5f24eb6554fdb9d1da79d924a122e08
Akash Dhami <asd15.sfu.ca>
2021-09-15 15:23:21 -0700

    Updating to add missing incomplete translations.

--- app.js
+++ app.js
@@ -7,21 +7,20 @@ const anvaad = require('anvaad-js');
 const app = express();
 
 
-app.get('/hukamnama.js', function(req, res){
+app.get('/hukamnama/', function (req, res) {
 
   const today = new Date();
   var month
   var date
   var year
 
-  if(Object.keys(req.query).length === 0)
-  {
+  if (Object.keys(req.query).length === 0) {
     console.log(today);
     month = (today.getMonth() + 1);
     date = today.getDate();
     year = (today.getYear() + 1900);
   }
-  else{
+  else {
     month = req.query.month;
     date = req.query.date;
     year = req.query.year;
@@ -38,75 +37,65 @@ app.get('/hukamnama.js', function(req, res){
     }
   }).then(response => {
     var conresponse = iconv.decode(response.body, 'windows-1252');
-     var $ = cheerio.load(conresponse);
-     var data = $('p','center').map(function()
-     {
-       return $(this).text();
-     }).get();
+    var $ = cheerio.load(conresponse);
 
-     var hukamnamaOrig = data[0].replace(/\n/g, '');
-     var hukamnamaUni = anvaad.unicode(hukamnamaOrig);
-     var punjabiTranslationOrig = data[1].replace(/\n/g,'');
-     var punjabiTranslationUni = anvaad.unicode(punjabiTranslationOrig);
-     var englishTranslation = data[2].replace(/\n/g,'');
+    //obtain paragraph data
+    var data = $('p', 'center').map(function () {
+      return $(this).text();
+    }).get();
 
-     var json = {
-       hukamnamaOrig: hukamnamaOrig,
-       hukamnamaUni: hukamnamaUni,
-       punjabiTranslationOrig: punjabiTranslationOrig,
-       punjabiTranslationUni: punjabiTranslationUni,
-       englishTranslation: englishTranslation,
-      };
+    //obtain missing hukamnama data
+    var data_b = $('strong', 'center').map(function () {
+      return $(this).text();
+    }).get();
+
+
+    //obtain missing punjabi translation data
+    var data_c = $('strong', 'span').map(function () {
+      return $(this).text();
+    }).get();
+
+    //obtain missing english translation data
+    var data_d = $('div', 'td').map(function () {
+      return $(this).text();
+    }).get();
+
+    //res.send(data_d[10]);
+
+    //prepare hukamnama data by adding data strings and removing '\n'
+    var hukamnamaData = data_b[1] + ' ' + data_b[2] + ' ' + data[0];
+    var hukamnamaOrig = hukamnamaData.replace(/\n/g, '');
+
+    //convert hukamana data to unicode
+    var hukamnamaUni = anvaad.unicode(hukamnamaOrig);
+
+
+    //prepare punjabi translation data
+    var punjabiTranslationData = data_c[0] + ' ' + data_c[1] + ' ' + data[1];
+    var punjabiTranslationOrig = punjabiTranslationData.replace(/\n/g, '');
+    var punjabiTranslationUni = anvaad.unicode(punjabiTranslationOrig);
+
+    //prepare english translation
+    var englishTranslationData = data_d[10] + ' ' + data_d[11] + ' ' + data[2];
+    var englishTranslation = englishTranslationData.replace(/\n/g, '');
+
+    var json = {
+      month: month,
+      date: date,
+      year: year,
+      hukamnamaOrig: hukamnamaOrig,
+      hukamnamaUni: hukamnamaUni,
+      punjabiTranslationOrig: punjabiTranslationOrig,
+      punjabiTranslationUni: punjabiTranslationUni,
+      englishTranslation: englishTranslation,
+    };
 
 
     res.send(json);
 
- }).catch(err => {
-     console.log(err);
- });
-
-
-
-
-
-
-
-  //old request library.
-  /* var options = {
-    url: 'https://old.sgpc.net/hukumnama/oldhukumnama.asp',
-    method: 'POST',
-    form: {
-      'month':month,
-      'date':date,
-      'year':year
-    }
-  } */
-
-  //console.log('options:', options);
-
-  /*
-  request(options, function(error, response, html) {
-    encoding:'binary';
-    if (error) {
-      console.log(error);
-    }
-    else {
-      console.log('html received: ');
-      //res.send(html)
-
-      var $ = cheerio.load(html, {decodeEntities: false});
-      var data = $('p','center').map(function()
-      {
-        return $(this).html();
-      }).get();
-
-      var hukamnama = data[0];
-      var punjabiTranslation = data[1];
-      var englishTranslation = data[2];
-
-      res.send(punjabiTranslation);
-    }
-  }); */
+  }).catch(err => {
+    console.log(err);
+  });
 
 });
 
